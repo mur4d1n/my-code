08baf88023ce54a2aaad5a4825c62e7eee82b25e
mur4d1n <80665048+mur4d1n@users.noreply.github.com>
2021-11-11 22:56:17 +0300

    Update HashTable.cpp

--- hash_table/HashTable.cpp
+++ hash_table/HashTable.cpp
@@ -1,15 +1,15 @@
 #include "HashTable.h"
 
-HashTable::HashTable(int remainder)
+HashTable::HashTable(int remainder) //Constructor for HashTable
 {
 	this->remainder = remainder;
-	this->values = std::vector<ListNode<int>*>(remainder);
+	this->values = std::vector<ListNode<int>*>(remainder); //I used my own version of linked list for this task
 
 	for (int i = 0; i < values.size(); i++)
 		values[i] = new ListNode<int>();
 }
 
-void HashTable::insert(int newValue)
+void HashTable::insert(int newValue) //This function inserts given value to HashTable objects into one of the many linked lists
 {
 	this->values[newValue % remainder]->insert(newValue);
 	return;
